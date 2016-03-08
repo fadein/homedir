72c1775128a758b6f507b9a36dde1f154bbb2808
U-APG\erikf <erikf@logerikfwin7.apg.local>
2016-03-08 11:41:54 -0700

    1st post
    The following repos are submodules:
       dotbash
       dotfiles
       dotscreen
       dottmux
       dotvim
       dotzsh

---
+++ README.md
@@ -0,0 +1,13 @@
+# Erik's homedir - just add water
+
+Meta repo which includes all of my other dotfile repos
+Just recursively clone this repo:
+
+``` git clone --recursive https://github.com/fadein/vimfiles.git ```
+
+or manually initialize the submodules after the fact:
+
+``` git submodule init; git submodule update ```
+
+
+Run ``` linkToHome.sh ``` to set up symlinks
---
+++ linkToHome.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# When the environment variable DRYRUN is non-empty, do not
+# actually make any changes, but only show what would be done
+
+set -e
+
+echodo() {
+	echo $@
+	[ 0"$DRYRUN" = "0" ] && $@ || true
+}
+
+# make sure that $HOME is defined
+if [ 0"$HOME" = "0" ]; then
+	echo "HOME is empty or unset!"
+	exit 1
+fi
+
+for L in */linkToHome.sh; do
+	[ -x $L ] && echodo $L
+done
